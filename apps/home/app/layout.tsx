dd181a8853f7c6c4f5884768a6914f461f0ccfb4
Aaron Miller <amiller@aps.org>
2024-02-12 03:52:19 +0000

    feat(home): add vercel analytics

--- apps/home/app/layout.tsx
+++ apps/home/app/layout.tsx
@@ -1,7 +1,9 @@
-import '@watheia/studio-ui/style.css';
+import { Analytics } from '@vercel/analytics/react';
 import { Metadata } from 'next';
 
 import { RootLayout } from '@watheia/studio-ui';
+
+import '@watheia/studio-ui/style.css';
 // import api from '../lib/content-api';
 
 export const metadata: Metadata = {
@@ -16,6 +18,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <Analytics />
       </body>
     </html>
   );
