19bce1685543698aa8a7575de62172710f606cd1
Aaron R Miller <amiller@aps.org>
2024-03-24 10:45:52 -0700

    feat(design-system): add storybook project (#4)
    * add design-system storybook project
    * add tailwindcss support
    * update studio-ui global style linkage
    * add story decorator

---
+++ apps/design-system/src/app/app.tsx
@@ -0,0 +1,14 @@
+import '@watheia/studio-ui/styles/index.css';
+import '../styles.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="design-system" />
+    </div>
+  );
+}
+
+export default App;
