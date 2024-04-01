5ffe8f26f71f02bb0c61198bc467b8e445e37c98
Aaron Miller <amiller@aps.org>
2024-04-01 03:08:16 +0000

    fix: playwright specs

--- apps/screenplay/src/index.spec.ts
+++ apps/screenplay/src/index.spec.ts
@@ -5,6 +5,6 @@ test('has title', async ({ page }) => {
 
   // Expect h1 to contain a substring.
   expect(await page.locator('h1').innerText()).toContain(
-    'Turn your ideas into reality with custom software by Watheia Labs'
+    'Turn your ideas into reality with custom software solutions'
   );
 });
