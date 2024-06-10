5d817d2ce20b0c85afd59ad1d0f6bd4e2915f68d
Aaron Miller <amiller@aps.org>
2024-06-10 07:07:39 +0000

    feat: add documentation app stubs

---
+++ apps/docs/tailwind.config.js
@@ -0,0 +1,15 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(__dirname, '{app,components,lib}/**/*!(*.stories|*.spec).{js,jsx,ts,tsx,md,mdx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  presets: [require('../../tailwind-workspace-presets.js')],
+};
