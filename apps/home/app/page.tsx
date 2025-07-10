24f6e0fff3a75a3325c0b15d9df86d33b4241373
Aaron Miller <amiller@aps.org>
2025-07-10 19:07:41 +0000

    chore: rename projects

--- apps/home/app/page.tsx
+++ apps/home/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome @watheia/home 👋
+              Welcome home 👋
             </h1>
           </div>
 
@@ -395,7 +395,7 @@ export default function Index() {
                 </svg>
                 View project details
               </summary>
-              <pre>nx show project @watheia/home --web</pre>
+              <pre>nx show project home --web</pre>
             </details>
             <details>
               <summary>
